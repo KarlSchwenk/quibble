e62667297d730927a0e43fbba637964e538872ce
Karl Schwenk <48796665+KarlSchwenk@users.noreply.github.com>
2022-04-01 15:38:53 +0200

    files for PyPi packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""Setup for packaging."""
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='quibble',
+    version='0.0.1dev',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/KarlSchwenk/quibble',
+    license='LGPL-2.1',
+    author='KarlSchwenk',
+    author_email='karl.max.schwenk@gmail.com',
+    description='Framework for Various Optimization Tasks',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['numpy==1.18.5']
+)
